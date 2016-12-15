ed4f2ca5aa8d5f8e07da8f9127ca5601def36e9a
Aaron Suarez <aaron.j.suarez1@gmail.com>
2016-12-15 02:56:19 +0000

    halfway thru billypediagit status!

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -3,14 +3,40 @@ $(document).ready(function() {
     $.getJSON('data.json', function (data) {
         // YOUR CODE BELOW HERE //
         
-        // uncomment this to inspect all available data; delete when done //
-        // console.log(data);
+        $('#section-bio').css('border', '2px solid blue').css('border-radius', '10px').css('margin', '10px').css('padding', '5px');
+        $('#section-praise').css('border', '2px solid white').css('border-radius', '10px').css('margin', '10px').css('padding', '5px');
+        $('#section-quotes').css('border', '2px solid black').css('border-radius', '10px').css('margin', '10px').css('padding', '5px');
         
-        // EXAMPLE: Looping over top rated recordings; replace with your code //
         let topRated = data.discography.topRated;
-        _.forEach(topRated, function(recording) {
-            console.log(recording);
+        let topRatedTitles = _.map(topRated, function(recording) {
+            return $('<li>').text(recording.title);
         });
+        $('#list-top-rated').append(topRatedTitles);
+        
+        $('<section>').attr('id', 'section-recordings').appendTo('#sidebar');
+        $('<ul>').attr('id', 'list-recordings').appendTo('#section-recordings');
+        
+        let recordings = data.discography.recordings;
+        let recordingListElements = _.map(recordings, function(track){
+            let $listElement = $('<li>').attr('class', 'recording');
+                $listElement.append($('<div>').attr('class', 'title').text('Title: ' + track.title));
+                $listElement.append($('<div>').attr('class', 'artist').text('Artist: ' + track.artist));
+                $listElement.append($('<div>').attr('class', 'release').text('Release: ' + track.release));
+                $listElement.append($('<div>').attr('class', 'year').text('Year: ' + track.year));
+            return $listElement;
+        });
+        console.log(recordingListElements);
+        $('#list-recordings').append(recordingListElements);
+        
+        let $divImageTopRated = $('<div>').attr('id', 'img-container-top-rated').attr('class', 'image-container');
+        let $topRatedImage = $('<img>').attr('id', 'top-rated-image').attr('class', 'image').attr('src', topRated[0].art);
+        $divImageTopRated.append($topRatedImage);
+        $('#header-top-rated').append($divImageTopRated);
+        
+        let $divImageRecording = $('<div>').attr('id', 'img-container-recording').attr('class', 'image-container');
+        let $recordingImage = $('<img>').attr('id', 'recording-image').attr('class', 'image').attr('src', recordings[0].art);
+        $divImageRecording.append($recordingImage);
+        $('#section-recordings').prepend($divImageRecording);
         
         
         // YOUR CODE ABOVE HERE //
